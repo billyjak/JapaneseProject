b1bce2d8d6305f15b76518b4a81c6ccad99cb012
billyjak <smellyjackgibson@gmail.com>
2023-04-16 17:17:50 -0400

    s

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 package org.example;
 
-import Practice.Practice;
-import Practice.TestPractice;
 import dao.Dao;
-import dao.TestDao;
+import practice.Practice;
+import userInterface.JFrameTest;
 
 import java.io.IOException;
-import java.util.Map;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        Dao dao = new Dao();
-        Practice practice = new Practice();
-        practice.practiceAmount(50);
+//        Practice practice = new Practice();
+        System.out.println("あいうえお");
+//        Dao dao = new Dao();
+//        JFrameTest jFrameTest = new JFrameTest();
+//        jFrameTest.createFrame();
     }
 }
---
+++ src/main/java/userInterface/JFrameTest.java
@@ -0,0 +1,30 @@
+package userInterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrameTest {
+    //1. Create the frame.
+
+    //2. Optional: What happens when the frame closes?
+    public void createFrame() {
+//1. Create the frame.
+        JFrame frame = new JFrame("FrameDemo");
+
+
+//2. Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//3. Create components and put them in the frame.
+//...create emptyLabel...
+
+        JLabel emptyLabel = new JLabel("食べます");
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+//4. Size the frame.
+        frame.pack();
+
+//5. Show it.
+        frame.setVisible(true);
+    }
+}
